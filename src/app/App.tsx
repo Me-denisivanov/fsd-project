d42f16628a720b74a646e7cc7f63b3a5370a9236
Denis Ivanov <>
2023-12-28 02:42:39 +0200

    Add sidebar; add layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,22 @@
 
 import { Suspense, } from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
 import { useTheme } from 'app/providers/ThemeProvider'
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
-
 import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames'
+import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme()
+  const { theme } = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>Change</button>
-
-      <Link to={'/'}>Main page</Link>
-      <Link to={'/about'}>About page</Link>
-
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <Navbar />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   )
 }
