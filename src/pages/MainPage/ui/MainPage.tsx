975434353da41bbf07ecae4c94c8787a0fd0381d
Denis Ivanov <>
2023-12-31 19:41:12 +0200

    add error boundary; add BundleAnalyzer

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
   const { t } = useTranslation();
 
   return (
-    <div>{t('Главная страница')}</div>
+    <div>
+      {t('Главная страница')}
+    </div>
   );
 };
 
