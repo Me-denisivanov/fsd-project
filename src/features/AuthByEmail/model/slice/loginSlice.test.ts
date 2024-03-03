75b8f5af021f308c95e8b4c1aa3fed52ce810835
ivanov <ivanov200212@gmail.com>
2024-03-03 23:21:20 +0200

    small fix

--- src/features/AuthByEmail/model/slice/loginSlice.test.ts
+++ src/features/AuthByEmail/model/slice/loginSlice.test.ts
@@ -5,14 +5,14 @@ import { loginActions, loginReducer } from './loginSlice';
 describe('loginSlice.test', () => {
   it('test set email', () => {
     const state: DeepPartial<LoginSchema> = { email: 'test@gmail.com' };
-    expect(loginReducer(state as LoginSchema, loginActions.setEmail('test@gmail.com'))).toBe({
+    expect(loginReducer(state as LoginSchema, loginActions.setEmail('test@gmail.com'))).toEqual({
       email: 'test@gmail.com',
     });
   });
 
   it('test set password', () => {
     const state: DeepPartial<LoginSchema> = { password: '1234' };
-    expect(loginReducer(state as LoginSchema, loginActions.setPassword('1234'))).toBe({
+    expect(loginReducer(state as LoginSchema, loginActions.setPassword('1234'))).toEqual({
       password: '1234',
     });
   });
