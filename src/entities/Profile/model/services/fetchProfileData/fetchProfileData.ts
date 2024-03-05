692cbdcffb68e620f443e2a5100c9aa026f018ea
ivanov <ivanov200212@gmail.com>
2024-03-05 22:26:29 +0200

    add types for fetchProfileData

--- src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -1,27 +1,20 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { ACCESS_TOKEN } from 'shared/const/localstorage';
 import { ThunkConfig } from 'app/providers/StoreProvider';
 import { Profile } from '../../types/profile';
 
 export const fetchProfileData = createAsyncThunk<Profile, void, ThunkConfig<string>>(
   'profile/fetchProfileData',
-  async (_, thunkApi) => {
-    const { extra, rejectWithValue } = thunkApi;
+  async (_, thunkAPI) => {
+    const { extra, rejectWithValue } = thunkAPI;
 
     try {
-      const accessToken = localStorage.getItem(ACCESS_TOKEN);
-
-      if (!accessToken) {
-        return [];
-      }
-
-      const response = await extra.api.get('/user/profile');
+      const response = await extra.api.get<Profile>('/user/profile');
 
       if (!response.data) {
         throw new Error();
       }
 
-      return response.data;
+      return response.data as Profile;
     } catch (error) {
       return rejectWithValue('Вы ввели неверный email или password');
     }
